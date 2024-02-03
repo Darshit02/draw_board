f75e660520c918941310d925fa890ff0e111f40c
LazY <darshitmistry2@gmail.com>
2024-02-03 20:19:24 +0530

    feat :search query push

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 
-import { UserButton } from "@clerk/nextjs"
+import { UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./search-input";
+
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-200">
-        <div className="hidden lg:flex lg:flex-1">
-            {/* TODO: Add A search button */}
-        </div>
-        <UserButton/>
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
+      <UserButton />
     </div>
-  )
-}
-
+  );
+};
