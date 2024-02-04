24952f078078b5d9186787043374d50a697534e0
LazY <darshitmistry2@gmail.com>
2024-02-04 19:17:47 +0530

    feat :empty state here

---
+++ app/(dashboard)/_components/empty-note.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export const EmptyNote = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+        <Image src="/note.svg"
+        alt="Empty"
+        width={110}
+        height={110} 
+        />
+        <h2 className="text-2xl font-semibold mt-6">
+            Create Your First Board
+        </h2>
+        <p className="text-muted-foreground text-sm mt-2">
+            Start by creating a board for your Organization
+        </p>
+        <div className="mt-6">
+          <Button size="lg">
+            Create Board
+          </Button>
+        </div>
+    </div>
+  )
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,27 @@
-const DashboardPage = () => {
-  return (
-    <div>Dash Board Root page</div>
-  )
-}
+"use client";
 
-export default DashboardPage
+import { BoardList } from "./_components/board-list";
+import { EmptyOrg } from "./_components/empty-org";
+import { useOrganization } from "@clerk/nextjs";
+
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
+
+  return (
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
+    </div>
+  );
+};
+
+export default DashboardPage;
