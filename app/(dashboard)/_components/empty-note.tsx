2adae2416dbac0222212673a9d88b70e5cfe3479
LazY <darshitmistry2@gmail.com>
2024-02-06 10:14:17 +0530

    fix: call api

--- app/(dashboard)/_components/empty-note.tsx
+++ app/(dashboard)/_components/empty-note.tsx
@@ -1,25 +1,32 @@
+"use client";
+import { useMutation } from "convex/react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
 
 export const EmptyNote = () => {
+  const { organization } = useOrganization();
+  const create = useMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    create({
+      orgId: organization.id,
+      title: "Untitled",
+    });
+  };
   return (
     <div className="h-full flex flex-col items-center justify-center">
-        <Image src="/note.svg"
-        alt="Empty"
-        width={110}
-        height={110} 
-        />
-        <h2 className="text-2xl font-semibold mt-6">
-            Create Your First Board
-        </h2>
-        <p className="text-muted-foreground text-sm mt-2">
-            Start by creating a board for your Organization
-        </p>
-        <div className="mt-6">
-          <Button size="lg">
-            Create Board
-          </Button>
-        </div>
+      <Image src="/note.svg" alt="Empty" width={110} height={110} />
+      <h2 className="text-2xl font-semibold mt-6">Create Your First Board</h2>
+      <p className="text-muted-foreground text-sm mt-2">
+        Start by creating a board for your Organization
+      </p>
+      <div className="mt-6">
+        <Button onClick={onClick} size="lg">Create Board</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
