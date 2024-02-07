45a8c78b522bcaf96f14afeee5326d3db91fdf29
LazY <darshitmistry2@gmail.com>
2024-02-07 11:21:33 +0530

    feat :list board,create board and then skeleton load

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,8 +1,12 @@
 "use client";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 import { EmptyFavorites } from "./empty-favorit";
 import { EmptyNote } from "./empty-note";
 import { EmptySearch } from "./empty-search";
+import { BoardCard } from "./boardcard";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -13,18 +17,58 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = []; // fetch data
+  const data = useQuery(api.boards.get, { orgId }); // fetch data
+
+  if (data === undefined) {
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {query.favorites ? "Favourite Boards" : "Team Boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
+  }
 
   if (!data?.length && query.search) {
     return <EmptySearch />;
   }
 
   if (!data?.length && query.favorites) {
-    return <EmptyFavorites/>;
+    return <EmptyFavorites />;
   }
   if (!data?.length) {
-    return <EmptyNote/>;
+    return <EmptyNote />;
   }
 
-  return <div>{JSON.stringify(query)}</div>;
+  return (
+    <div>
+      <h2 className="text-3xl">
+        {query.favorites ? "Favourite Boards" : "Team Boards"}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
