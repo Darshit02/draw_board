ac62edbc35b61a68feea1e021f31802e543c28bd
LazY <darshitmistry2@gmail.com>
2024-02-11 17:19:50 +0530

    build: liveblock config inited

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,7 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
 export const Info = () => {
   return (
     <div className="absolute top-2 left-2 bg-white  rounded-md px-1.5 h-12 flex items-center shadow-md">
         
     </div>
+  )
+}
+
+
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className="animate-pulse absolute top-2 left-2 bg-neutral-100 rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]"/>
   )
 }
--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,6 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton"
 
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex shadow-md items-center">Participants</div>
+  )
+}
+
+
+Participants.Skeleton = function ParticipantsSkeleton() {
+  return (
+    <div className="animate-pulse absolute h-12 top-2 right-2 bg-neutral-100 rounded-md p-3 flex shadow-md items-center w-[100px]"/>
   )
 }
